6bfb8c0b34a1f4a66a866bb561ddcba047850356
Connor Rosenberg <crosenberg8@gatech.edu>
2021-08-21 21:05:57 -0400

    Added tag feature (without the submit buttoon yet)

--- src/components/FormSection/FormSection.jsx
+++ src/components/FormSection/FormSection.jsx
@@ -2,9 +2,13 @@ import React, {useState} from 'react'
 import "./FormSection.css"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css";
+import { UserTag } from '../ItemSection/ItemSectionElements';
 
 const FormSection = () => {
     const [startDate, setStartDate] = useState(new Date());
+    const userTags = ["Hello", "Bill", "Example"];
+
+    
 
     return (
         <div className="form-background">
@@ -16,16 +20,21 @@ const FormSection = () => {
                 </label>
                 
                 <label>
-                    Tags: <input type="text" name="todoTags" /><button>Create new tag</button>
+                    Tags: <input type="text" name="todoTags"/> <button>Create new tag</button>
                 </label>
                 <div className="todoTagsDisplayed">
                     {/* Here, I will use react to show the different tags */}
+                    {userTags.map((value, index) => {
+                        return (
+                            <UserTag id={index}>{value}</UserTag>
+                        )
+                    })}
                 </div>
                 <label>
                     Due Date: <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} placeholderText="mm/dd/yyyy"/>
                 </label>
                 
-                <button>Create Item</button>
+                <button type="submit">Create Item</button>
             </form>
         </div>
     )
---
+++ src/components/ItemSection/ItemSectionElements.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const UserTag = styled.div`
+    background: #000;
+    color: #fff;
+    padding: 5px;
+    margin: 5px;
+    border-radius: 5px;
+`
